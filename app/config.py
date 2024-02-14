434157011315d52750b103334afa4d15beb45140
Jahn <jpdeoliveiraferreira@gmail.com>
2024-02-14 09:52:14 -0300

    :sparkles: Add settings class

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):  # type: ignore
+    minio_host: str
+    minio_access_key: str
+    minio_secret_key: str
+    minio_bucket_name: str
