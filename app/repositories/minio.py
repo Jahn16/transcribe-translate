ae6a9e50bda5a22ac6e4c4edf1f0656258cc7572
Jahn <jpdeoliveiraferreira@gmail.com>
2024-02-14 10:07:39 -0300

    :sparkles: Add minio upload logic

--- app/repositories/minio.py
+++ app/repositories/minio.py
@@ -1,13 +1,15 @@
 import abc
 from typing import BinaryIO
 
+from minio import Minio
+
 from app.config import Settings
 
 
 class BaseRepository(abc.ABC):
     @abc.abstractmethod
     def upload_file(
-        self, data: BinaryIO, file_name: str, file_size: int
+        self, file_name: str, data: BinaryIO, file_size: int
     ) -> None:
         raise NotImplementedError
 
@@ -18,12 +20,20 @@ class BaseRepository(abc.ABC):
 
 class MinioRepository(BaseRepository):
     def __init__(self, settings: Settings):
-        pass
+        self._client = Minio(
+            settings.minio_host,
+            access_key=settings.minio_access_key,
+            secret_key=settings.minio_secret_key,
+        )
+        self._bucket_name = settings.minio_bucket_name
 
     def upload_file(
-        self, data: BinaryIO, file_name: str, file_size: int
+        self, file_name: str, data: BinaryIO, file_size: int
     ) -> None:
-        pass
+        if not self._client.bucket_exists(self._bucket_name):
+            self._client.make_bucket(self._bucket_name)
+
+        self._client.put_object(self._bucket_name, file_name, data, file_size)
 
     def get_presigned_url(self, file_name: str) -> str:
-        return ""
+        return self._client.presigned_get_object(self._bucket_name, file_name)
