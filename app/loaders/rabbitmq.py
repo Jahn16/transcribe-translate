3bc8e14dcda93145f8161cc828dd9ef55c6092b4
Jahn <jpdeoliveiraferreira@gmail.com>
2024-02-19 18:11:51 -0300

    :sparkles: Implement rabbitmq logic

--- app/loaders/rabbitmq.py
+++ app/loaders/rabbitmq.py
@@ -1,5 +1,7 @@
 import abc
 
+from aio_pika import Message, connect
+
 from app.config import Settings
 
 
@@ -14,7 +16,15 @@ class BaseMessageSender(abc.ABC):
 
 class MessageSender(BaseMessageSender):
     def __init__(self, settings: Settings) -> None:
-        pass
+        self._url = settings.rabbitmq_url
+        self._queue_name = settings.rabbitmq_queue_name
 
     async def send(self, message: str) -> None:
-        pass
+        connection = await connect(self._url)
+        async with connection:
+            channel = await connection.channel()
+            queue = await channel.declare_queue(self._queue_name)
+            await channel.default_exchange.publish(
+                Message(bytes(message, "utf-8")),
+                routing_key=queue.name,
+            )
