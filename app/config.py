e817445f087752cf5bb64083ac464a22741e3dcd
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-03-29 15:43:58 -0300

    🩹 Remove default value for whisper device

--- app/config.py
+++ app/config.py
@@ -3,6 +3,4 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):  # type: ignore
     whisper_model: str = "small"
-    whisper_device: str = ""
-
-
+    whisper_device: str
