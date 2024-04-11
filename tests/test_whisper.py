cd0acdf101e09718988859aa9a0ab0bdfe686c4a
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-04-11 16:33:52 -0300

    ✅ Update whisper test

--- tests/test_whisper.py
+++ tests/test_whisper.py
@@ -13,14 +13,24 @@ def settings() -> Settings:
 
 
 @pytest.fixture
+def model_size() -> str:
+    return "small"
+
+
+@pytest.fixture
+def device() -> str:
+    return "cpu"
+
+
+@pytest.fixture
 def audio() -> str:
     return "audio.mp3"
 
 
 def test_translate(
-    audio: str, settings: Settings, mocker: MockerFixture
+    audio: str, model_size: str, device: str, mocker: MockerFixture
 ) -> None:
-    whisper = Whisper(settings)
+    whisper = Whisper(model_size, device)
     whisper_mock = mocker.patch("app.providers.whisper.WhisperModel")
     segments_mock, info_mock = ([MagicMock()], MagicMock())
     lang = "de"
@@ -28,7 +38,7 @@ def test_translate(
     whisper_mock().transcribe.return_value = (segments_mock, info_mock)
     result = whisper.transcribe(audio)
     whisper_mock.assert_called_with(
-        settings.whisper_model, device=settings.whisper_device
+        model_size, device=device
     )
     whisper_mock().transcribe.assert_called_once_with(audio)
     assert result == (lang, segments_mock)
