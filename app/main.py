46daf83ce2c7977103c5d02f2fa3735150f5851b
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-04-23 17:02:22 -0300

    ✨ Wire up tasks route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI
 
-from app.routers import speech_to_text
+from app.routers import speech_to_text, tasks
 from app.utils.logging import setup_logging
 
 setup_logging()
@@ -10,5 +10,6 @@ app = FastAPI()
 api_router = APIRouter(prefix="/api/v1")
 
 api_router.include_router(speech_to_text.router)
+api_router.include_router(tasks.router)
 
 app.include_router(api_router)
