b156368bd300a7acc34cc788a5c717f25fa53acf
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-04-23 16:55:22 -0300

    ✨ Add celery app

---
+++ app/celery.py
@@ -0,0 +1,29 @@
+from celery import Celery
+
+from app.providers.translate import ArgosTranslate
+from app.use_cases.transcribe import TranscribeUseCase
+from app.use_cases.translate import TranslateUseCase
+
+app = Celery(
+    "tasks",
+    broker="redis://localhost:6379/0",
+    backend="redis://localhost:6379/0",
+)
+
+
+@app.task
+def transcribe(
+    whisper_model: str, whisper_device: str, audio_path: str
+) -> tuple[str, str]:
+    use_case = TranscribeUseCase(whisper_model, whisper_device)
+    result = use_case.execute(audio_path)
+    return result
+
+
+@app.task
+def translate(input: tuple[str, str], to_lang: str):
+    from_lang, sentences = input
+    # to_lang = "pt"
+    translator = ArgosTranslate()
+    use_case = TranslateUseCase(translator)
+    return use_case.execute(sentences, from_lang, to_lang)
