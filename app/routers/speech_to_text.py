e00aa50c94b852b63408241a69d1b04fd1a18f7d
Jahn16 <jpdeoliveiraferreira@gmail.com>
2024-04-23 17:00:46 -0300

    ♻️ Using celery to call tasks

--- app/routers/speech_to_text.py
+++ app/routers/speech_to_text.py
@@ -1,12 +1,36 @@
+import tempfile
 from typing import Annotated
 
-from fastapi import APIRouter, Form, UploadFile
+from celery import chain
+from fastapi import APIRouter, Depends, Form, UploadFile
+from fastapi.responses import JSONResponse
+
+from app.celery import transcribe, translate
+from app.config import Settings
+from app.deps import get_settings
 
 router = APIRouter(prefix="/speech_to_text")
 
 
 @router.post("/")
-async def speech_to_text(
-    file: UploadFile, target_language: Annotated[str, Form()]
-) -> dict[str, bool]:
-    return {"sucess": True}
+def speech_to_text(
+    file: UploadFile,
+    target_language: Annotated[str, Form()],
+    settings: Settings = Depends(get_settings),
+) -> JSONResponse:
+    audio_file = tempfile.NamedTemporaryFile(delete=False)
+    audio_file.write(file.file.read())
+    audio_file.close()
+
+    # task = transcribe.delay(  # type: ignore
+    #     settings.whisper_model, settings.whisper_device, audio_file.name
+    # )
+    res = chain(
+        transcribe.s(
+            settings.whisper_model, settings.whisper_device, audio_file.name
+        ),
+        translate.s(target_language),
+    )()
+    return JSONResponse(
+        {"transcription_task": res.parent.id, "translation_task": res.id}
+    )
